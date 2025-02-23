edf0dfd2cc31ad636a16b268e6a96c7fb986e306
Abdelillah <abdelillahx9@gmail.com>
2025-02-23 22:54:28 +0100

    Fixed routing issues for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,29 @@
 import { Route, Routes } from "react-router-dom";
-
 import React from 'react';
 import './index.css';
-
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectPage from "./pages/ProjectPage";
 import SkillPage from "./pages/SkillPage";
 import ContactPage from "./pages/ContactPage";
 
-
-
-
-
-
-
 const App = () => {
   return (
-
     <div className='h-screen w-screen bg-gray-900 text-gray-100 overflow-auto'>
-
-
       <Navbar />
-
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/projects' element={<ProjectPage />} />
-        <Route path='/skills' element={<SkillPage />} />
-        <Route path='/contact' element={<ContactPage />} />
-
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/skills" element={<SkillPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
-
-
-
-
-
       <Footer />
-
     </div>
   );
 };
 
 export default App;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-import { HashRouter } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// Get the root element
 const rootElement = document.getElementById('root');
 
-// Check if the root element exists
 if (rootElement) {
   const root = createRoot(rootElement);
-
   root.render(
     <StrictMode>
       <HashRouter>
